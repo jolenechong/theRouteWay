55f48b377db40704aea570ab79df822a32150468
jolenechong <jolenechong7@gmail.com>
2022-10-01 09:39:02 +0800

    dynamically get whats in ./data/roads/ and show according to whats in array on route page

--- client/src/pages/route/index.tsx
+++ client/src/pages/route/index.tsx
@@ -4,8 +4,10 @@ import Form from '../../components/form';
 import RoutesBox from '../../components/routesBox';
 // @ts-ignore
 import map from '../../data/map.png';
-// @ts-ignore
-import road1n2 from '../../data/roads/Road 1 and 2.png';
+
+interface Object {
+  [key: string]: string;
+}
 
 function GetRoute() {
 
@@ -17,6 +19,24 @@ function GetRoute() {
 
     const source = searchParams.get('source') as string;
     const destination = searchParams.get('destination') as string;
+
+    function importAll(r: any) {
+      let finalDict = {} as Object
+      const keys = r.keys()
+      const values = r.keys().map(r)
+      
+      for (let i=0; i <keys.length; i++){
+          const key = keys[i].split("/")[1]
+          if (key !== undefined){
+              finalDict[key.split('.')[0]] = values[i]
+          }
+      }
+
+      return finalDict;
+  }
+  const roads = importAll(require.context('../../data/roads/', true, /\.(png|jpe?g|svg)$/));
+  const roadsToShow = [1]
+  console.log(roads)
 
   return (
     <>
@@ -39,6 +59,7 @@ function GetRoute() {
         // display one result map view
         <div className='tw-flex tw-h-screen tw-w-screen'>
         <div className="tw-shadow tw-p-4 tw-w-[20%]">
+            <button onClick={(() => setDisplayDir(false))}>&#60; Back</button>
             <h1 className="tw-pt-8">Map View</h1>
             <p>From {source} to {destination}.</p>
             <p className='tw-font-bold tw-text-xl'>Option {0}</p>
@@ -47,13 +68,14 @@ function GetRoute() {
             {route.map((road, index) => {
                 return <p key={index}>{road}</p>
             })}
-
-
         </div>
         <div className='tw-shadow tw-w-[80%]'>
           <div className='tw-relative tw-w-full tw-h-screen'>
             <img src={map} alt='map' className='tw-h-screen tw-w-full tw-overflow-scroll tw-absolute tw-top-0 tw-right-0'/>
-            <img src={road1n2} alt='map' className='tw-h-screen tw-w-full tw-overflow-scroll tw-absolute tw-top-0 tw-right-0'/>
+            {roadsToShow.map((road, index) => {
+              return <img src={roads[road]} alt='road' className='tw-h-screen tw-w-full tw-overflow-scroll tw-absolute tw-top-0 tw-right-0'/>
+            })}
+
           </div>
         </div>
     </div>}
