57c528f4f0de620b870edbb884edc370e353d02d
jolenechong <jolenechong7@gmail.com>
2022-10-01 09:07:48 +0800

    experimenting with layering images, need to add scroll

--- client/src/components/routesBox/index.tsx
+++ client/src/components/routesBox/index.tsx
@@ -10,38 +10,14 @@ interface IProps {
     timeNeeded: number,
     delay: Boolean,
     delayTime?: number, //optional
+    setDisplayDir: React.Dispatch<React.SetStateAction<boolean>>
 }
 
-function RoutesBox({ option, route, timeStart, timeEnd, destination, source, timeNeeded, delay, delayTime }: IProps) {
-
-    const [displayDir, setDisplayDir] = useState(false);
-
-    const handleClick = () => {
-
-    }
+function RoutesBox({ option, route, timeStart, timeEnd, destination, source, timeNeeded, delay, delayTime, setDisplayDir }: IProps) {
 
     return (
         <>
             <>
-                {
-                    displayDir ?
-                        <div>
-                            <div className="tw-shadow tw-p-4">
-                                <h1 className="tw-pt-8">Map View</h1>
-                                <p>From {source} to {destination}.</p>
-                                <p className='tw-font-bold tw-text-xl'>Option {option}</p>
-                                <p className='tw-font-bold tw-text-lg'>{timeStart}: {timeEnd}</p>
-                                <p className='tw-font-bold tw-text-lg'>Roads to Pass:</p>
-                                {route.map((road, index) => {
-                                    return <p key={index}>{road}</p>
-                                })}
-
-
-                            </div>
-                            <div className='tw-shadow tw-w-[70%] tw-p-4'>
-                            </div>
-                        </div>
-                        :
                         <div className='tw-rounded-xl tw-bg-blue-100 tw-p-6 tw-relative tw-mt-2'>
                             <p>Option {option}</p>
                             <p className='tw-font-bold tw-text-lg'>{timeStart}: {timeEnd}</p>
@@ -54,8 +30,6 @@ function RoutesBox({ option, route, timeStart, timeEnd, destination, source, tim
                             {delay ? <p className='tw-text-red-600'>*Potential Delays due to Traffic Jams</p> : <></>}
                             <button className='tw-underline tw-outline-none tw-bg-none tw-absolute tw-bottom-6 tw-right-6' onClick={(() => setDisplayDir(true))}>Get Directions</button>
                         </div>
-                }
-
 
             </>
         </>
--- client/src/pages/route/index.tsx
+++ client/src/pages/route/index.tsx
@@ -2,11 +2,17 @@ import { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import Form from '../../components/form';
 import RoutesBox from '../../components/routesBox';
+// @ts-ignore
+import map from '../../data/map.png';
+// @ts-ignore
+import road1n2 from '../../data/roads/Road 1 and 2.png';
 
 function GetRoute() {
 
     const [searchParams, setSearchParams] = useSearchParams();
     const [route, routes] = useState([]);
+    const [displayDir, setDisplayDir] = useState(false);
+
     // TODO: initialise routes from algo here, use useeffect
 
     const source = searchParams.get('source') as string;
@@ -15,6 +21,8 @@ function GetRoute() {
   return (
     <>
         <>
+        { !displayDir ? 
+        // if dw display yet den show all results
         <div className='tw-flex tw-justify-center tw-p-10'>
         <div className='tw-flex-1'>
         <Form source={source} destination={destination}/>
@@ -23,10 +31,32 @@ function GetRoute() {
             <h1>Retrieveing Your Best Routes...</h1>
             {/* only load this part after submitting form and when it returns data */}
             {/* do mapping frm a list and use this component a few times */}
-            <RoutesBox option={0} timeStart={'6.43pm'} timeEnd={'7.26pm'} destination={destination} source={source} timeNeeded={40} delay={false} route={[]} />
-            <RoutesBox option={0} timeStart={'6.43pm'} timeEnd={'7.26pm'} destination={destination} source={source} timeNeeded={40} delay={true} delayTime={5} route={[]}/>
+            <RoutesBox option={0} timeStart={'6.43pm'} timeEnd={'7.26pm'} destination={destination} source={source} timeNeeded={40} delay={false} route={[]} setDisplayDir={setDisplayDir}/>
+            <RoutesBox option={0} timeStart={'6.43pm'} timeEnd={'7.26pm'} destination={destination} source={source} timeNeeded={40} delay={true} delayTime={5} route={[]} setDisplayDir={setDisplayDir}/>
         </div>
         </div>
+        :
+        // display one result map view
+        <div className='tw-flex tw-h-screen tw-w-screen'>
+        <div className="tw-shadow tw-p-4 tw-w-[20%]">
+            <h1 className="tw-pt-8">Map View</h1>
+            <p>From {source} to {destination}.</p>
+            <p className='tw-font-bold tw-text-xl'>Option {0}</p>
+            <p className='tw-font-bold tw-text-lg'>{0}: {0}</p>
+            <p className='tw-font-bold tw-text-lg'>Roads to Pass:</p>
+            {route.map((road, index) => {
+                return <p key={index}>{road}</p>
+            })}
+
+
+        </div>
+        <div className='tw-shadow tw-w-[80%]'>
+          <div className='tw-relative tw-w-full tw-h-screen'>
+            <img src={map} alt='map' className='tw-h-screen tw-w-full tw-overflow-scroll tw-absolute tw-top-0 tw-right-0'/>
+            <img src={road1n2} alt='map' className='tw-h-screen tw-w-full tw-overflow-scroll tw-absolute tw-top-0 tw-right-0'/>
+          </div>
+        </div>
+    </div>}
         </>
     </>
   );
